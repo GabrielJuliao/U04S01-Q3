85928959a0579e13f4a0fa8b545994806270dd37
GabrielJuliao <gabriel360b@gmail.com>
2019-05-28 16:03:26 -0300

    Uso Da Interface

--- src/Q3/ConstantesMatematicas.java
+++ src/Q3/ConstantesMatematicas.java
@@ -1,5 +1,8 @@
 package Q3;
 
 public interface ConstantesMatematicas {
-
+	public static final double raizDe2 = 1.4142135623730950488;
+	public static final double raizDe3 = 1.7320508075688772935; 
+	public static final double raizDe5 = 2.2360679774997896964;
+	public static final double raizDe6 = raizDe2*raizDe3; 
 }
---
+++ src/Q3/Main.java
@@ -0,0 +1,13 @@
+package Q3;
+
+public class Main implements ConstantesMatematicas{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("raiz de dois = "+raizDe2);
+System.out.println("raiz de tres = "+raizDe3);
+System.out.println("raiz de cinco = "+raizDe5);
+System.out.println("raiz de seis = "+raizDe6);
+	}
+
+}
